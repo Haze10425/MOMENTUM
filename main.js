ec911dbe862a32165304ba4459d21178f6c24d0a
Haze10425 <jheee042500@gmail.com>
2023-02-16 23:19:25 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("HELLO WORLD!");
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek);
+
+// Get Item from Array
+console.log(daysOfWeek[0]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
